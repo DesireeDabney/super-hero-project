acf4d3aa2b73a55a75d7dc6ac39027bffd67abad
Desiree DabneyCorona <ddabne916@west-mec.org>
2025-10-07 13:55:18 -0700

    Structure files and folders

---
+++ server.js
@@ -0,0 +1,14 @@
+// Main Express server(backend)
+// Routes: GET /, GET / heroes, POST / heroes,
+// PUT /heroes/:id, DELETE /heroes/:id
+
+// GET
+
+// GET HEROES
+
+// POST HEROES
+
+// PUT HEROES
+
+// DELETE HEROES
+
