1586c9e4ba4e843092741c2878461d89ee4290a2
Damian Borbolla <dborbo047@west-mec.org>
2025-10-08 14:03:11 -0700

    committing form-input.ejs, heroForm.ejs and heroInputs.config.js

---
+++ config/heroInputs.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  superName: {
+    id: "superName",
+    name: "superName",
+    label: "Super Name",
+    type: "text",
+    required: true,
+    placeholder: "Superman",
+  },
+  realName: {
+    id: "realName",
+    name: "realName",
+    label: "Real Name",
+    type: "text",
+    required: true,
+    placeholder: "Clark Kent",
+  },
+  superpower: {
+    id: "superpower",
+    name: "superpower",
+    label: "Superpower",
+    type: "text",
+    required: true,
+    placeholder: "Laser Vision",
+  }
+};
